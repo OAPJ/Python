f6d899f8a8e40542c7241503a88ce0abdb5eb24b
Juan Ovalle <ovallejuan98@gmail.com>
2019-01-02 18:58:11 -0600

    Se agrego el NestedLists

--- HackerRank/BasicDataTypes/NestedLists.py
+++ HackerRank/BasicDataTypes/NestedLists.py
@@ -2,24 +2,27 @@ if __name__ == '__main__':
 
     clase = []
     mas_bajo = 100
-    segundo_mas_bajo2 = 0
+    segundo_mas_bajo2 = 99
     for _ in range(int(input())):
         name = input()
         score = float(input())
         lista2 = [name,score]
         clase.append(lista2)
-        if _ == 0:
-            mas_bajo = lista2[1]
-            print(str(mas_bajo)+" "+str(segundo_mas_bajo2))
-        elif lista2[1] < mas_bajo:
-            if mas_bajo != segundo_mas_bajo2 :
-                print(str(mas_bajo)+" "+str(segundo_mas_bajo2))
-                segundo_mas_bajo2 = mas_bajo
-                lista3 = [lista2]
-            elif mas_bajo == segundo_mas_bajo2:
-                print(str(mas_bajo)+" "+str(segundo_mas_bajo2))
-                lista3.append(lista2)
-            mas_bajo = lista2[1]
+        if score < mas_bajo:
+            mas_bajo = score
 
+    lista3 = []
+    contador = 0;
+    for i in range(len(clase)):
+        if clase[i][1] > mas_bajo and clase[i][1] == segundo_mas_bajo2:
+            lista3.insert(contador,clase[i][0])
+            contador+=1
+        elif clase[i][1] > mas_bajo and clase[i][1] < segundo_mas_bajo2:
+            segundo_mas_bajo2 = clase[i][1]
+            lista3 = []
+            lista3.insert(0,clase[i][0])
+            contador+=1
 
-print(lista3)
+    lista3.sort()
+    for i in range(len(lista3)):
+        print(lista3[i])
